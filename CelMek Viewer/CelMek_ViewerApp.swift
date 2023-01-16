56a890cfb1b641715c091531b8e430631a15e6f7
Mattias Holm <lorrden@openorbit.org>
2023-01-16 07:07:26 +0100

    Initial Commit

---
+++ CelMek Viewer/CelMek_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelMek_ViewerApp.swift
+//  CelMek Viewer
+//
+//  Created by Mattias Holm on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct CelMek_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CelMek ViewerUITests/CelMek_ViewerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CelMek_ViewerUITestsLaunchTests.swift
+//  CelMek ViewerUITests
+//
+//  Created by Mattias Holm on 2023-01-16.
+//
+
+import XCTest
+
+final class CelMek_ViewerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
